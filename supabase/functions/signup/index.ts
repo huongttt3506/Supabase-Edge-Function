44c2f4509fe3192b7af12ebb074e5ceb0e861f36
huongttt3506 <huongttt.ueh@gmail.com>
2025-01-09 16:10:00 +0900

    sign up function

--- supabase/functions/signup/index.ts
+++ supabase/functions/signup/index.ts
@@ -4,19 +4,51 @@
 
 // Setup type definitions for built-in Supabase Runtime APIs
 import "jsr:@supabase/functions-js/edge-runtime.d.ts"
+import { corsHeaders } from "../_shared/cors.ts"
+import { createClient } from 'jsr:@supabase/supabase-js@2'
 
-console.log("Hello from Functions!")
+console.log("Hello from signup Functions!")
 
 Deno.serve(async (req) => {
-  const { name } = await req.json()
-  const data = {
-    message: `Hello ${name}!`,
+  if (req.method === 'OPTION') {
+    return new Response('ok', { headers: corsHeaders })
   }
 
-  return new Response(
-    JSON.stringify(data),
-    { headers: { "Content-Type": "application/json" } },
-  )
+  try {
+    const { email, password, confirmPassword, address } = await req.json();
+
+    // check password
+    if (!password || password !== confirmPassword) {
+      throw new Error('Invalid or mismatched password!')
+    }
+    const supabaseClient = createClient(
+      Deno.env.get('SUPABASE_URL') ?? '',
+      Deno.env.get('SUPABASE_ANON_KEY') ?? '',
+      { global: { headers: { Authorization: req.headers.get('Authorization')! } } }
+    )
+  
+
+    //create new user
+    const { user, error } = await supabaseClient.auth.signUp({
+      email,
+      password,
+      address,
+    })
+
+    if (error) {throw new Error(error.message)}
+
+    return new Response(JSON.stringify({message: 'User signed up successfully!', user }), {
+      headers: { ...corsHeaders, 'Content-Type': 'application/json' },
+      status: 200,
+    })
+  } catch (error) {
+    const err = error as Error
+    return new Response(JSON.stringify({error: err.message}), {
+      headers: { ...corsHeaders, 'Content-Type': 'application/json' },
+      status: 400,
+    })
+  }
+
 })
 
 /* To invoke locally:
